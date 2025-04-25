3a47de995e550d4dd942755e21b1775c6581471d
shiron <shiron4710.dev@gmail.com>
2025-04-25 14:02:46 +0900

    feat(audio): Update audio format handling in SpeakerModule to use dynamic format from parameters

--- modules/audio/speaker.py
+++ modules/audio/speaker.py
@@ -35,28 +35,29 @@ class SpeakerModule(AkariModule):
 
         buffer = io.BytesIO(audio.main)
 
-        audio_segment = AudioSegment.from_file(buffer, format="mp3")
+        audio_segment = AudioSegment.from_file(buffer, format=params.format)
         wav_io = io.BytesIO()
         audio_segment.export(wav_io, format="wav")
         wav_io.seek(0)
-
         with wave.open(wav_io, "rb") as wf:
             p = pyaudio.PyAudio()
-            stream = p.open(
-                format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True,
-            )
+            try:
+                stream = p.open(
+                    format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True,
+                )
 
-            audio_data = wf.readframes(1024)
-            while audio_data:
-                stream.write(audio_data)
                 audio_data = wf.readframes(1024)
+                while audio_data:
+                    stream.write(audio_data)
+                    audio_data = wf.readframes(1024)
 
-            stream.stop_stream()
-            stream.close()
-            p.terminate()
+                stream.stop_stream()
+                stream.close()
+            finally:
+                p.terminate()
 
         dataset = AkariDataSet()
         return dataset
