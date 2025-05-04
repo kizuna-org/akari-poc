2b2bb7845e3be5d988b13bba63c6a134a9efe6f2
shiron <shiron4710.dev@gmail.com>
2025-05-04 22:37:11 +0900

    fix(print): update logging level from debug to info for better visibility of last data

--- modules/print.py
+++ modules/print.py
@@ -21,32 +21,23 @@ class _PrintModule(AkariModule):
         self._logger.debug("Data: %s", data)
         self._logger.debug("Params: %s", params)
 
-        try:
-            self._logger.debug("Last Data: %s", json.dumps(data.last(), indent=4))
-        except:
-            self._logger.debug("Last Data: %s", repr(data.last()))
-
-        return AkariDataSet()
-
-    def stream_call(self, data: AkariData, params: Any, callback: AkariModuleType | None = None) -> AkariDataSet:
-        self._logger.debug("PrintModule stream_call called")
-        self._logger.debug("Data: %s", data)
-        self._logger.debug("Params: %s", params)
-
         last = data.last()
         try:
-            self._logger.debug("Last Data: %s", json.dumps(last, indent=4))
+            self._logger.info("Last Data: %s", json.dumps(last, indent=4))
         except:
-            self._logger.debug("Last Data: %s", last)
+            self._logger.info("Last Data: %s", last)
 
         for field in last.__dict__:
             if hasattr(last, field):
                 value = getattr(last, field)
                 if isinstance(value, AkariDataSetType):
                     if value.main is not None:
-                        self._logger.debug("%s: %s", field, value.main)
+                        self._logger.info("%s: %s", field, value.main)
                 else:
                     if value is not None:
-                        self._logger.debug("%s: %s", field, value)
+                        self._logger.info("%s: %s", field, value)
 
-        return AkariDataSet()
+        return data.last()
+
+    def stream_call(self, data: AkariData, params: Any, callback: AkariModuleType | None = None) -> AkariDataSet:
+        return self.call(data, params, callback)
